884e3f14e8bce21f53ed6a9d081243b09ddd5a90
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 08:42:41 +0000

    Deploy STEP-tw/testgol to github.com/STEP-tw/testgol.git:gh-pages

---
+++ precache-manifest.017149902470a5174b1ab048f27277a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a0688e15e5438bc4fb",
+    "url": "/testgol/static/js/runtime~main.c9a0688e.js"
+  },
+  {
+    "revision": "e488b6d79f78be2c0507",
+    "url": "/testgol/static/js/main.e488b6d7.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/testgol/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "e488b6d79f78be2c0507",
+    "url": "/testgol/static/css/main.cdaa3718.chunk.css"
+  },
+  {
+    "revision": "9dca29b963ba7d61b2e45d93cbe51869",
+    "url": "/testgol/index.html"
+  }
+];
